0dccc1bdf4712d59726083d1245d59100197e496
Alberto <albertoue93@gmail.com>
2020-09-15 20:39:35 -0600

    Third Commit

--- src/component/Hola.js
+++ src/component/Hola.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Hola extends Component {
     render() {
         return(
+		<>
             <h1>Hola Mundo</h1>
+			<footer>Alberto Urbina Espinoza</footer>
+		</>
         );
     }
 }
